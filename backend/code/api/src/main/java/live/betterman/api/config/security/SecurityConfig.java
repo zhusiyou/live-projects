2513babb9a0adea03e882bfefdb7ec471fcdb388
zhusiyou <ifengforever@outlook.com>
2024-01-05 10:37:54 +0800

    测试自定义数据源

--- backend/code/api/src/main/java/live/betterman/api/config/security/SecurityConfig.java
+++ backend/code/api/src/main/java/live/betterman/api/config/security/SecurityConfig.java
@@ -1,8 +1,15 @@
 package live.betterman.api.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author: zhudawei
@@ -28,5 +35,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .clearAuthentication(true)
                 .logoutSuccessUrl("/test/")
         ;
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManager) throws Exception{
+        authenticationManager.userDetailsService(userDetailsService());
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        UserDetails userDetails = User.withUsername("haha").password("{noop}haha").roles("guess").build();
+        manager.createUser(userDetails);
+        return manager;
     }
 }
